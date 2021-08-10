8c87995425c131dcf002b3dd9d065a70bb655fd4
Demarcus Lester <demarcuslester12@gmail.com>
2021-08-10 10:15:12 -0500

    Notes for charts

---
+++ java.js
@@ -0,0 +1,4 @@
+// console.log("Sanity...");
+// we will be checking for the dat we collect and apply it to a chart, we have image issues so we can check our code, but try to keep up
+// shy away from kevins dupe method if possible, but this is based on what he did
+// the way we did our java gathering of new scores, is doing a bulk of the work, so we are 
